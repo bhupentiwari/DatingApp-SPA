fe95f7a497a11bd4d398f6cf094338c810369a7c
Tiwari <bhupendrakumar.tiwari@getronics.com>
2018-12-11 13:23:58 +0100

    End of Section 4

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_service/auth.service';
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  Login() {
+
+    this.authService.login(this.model).subscribe(next => {
+        console.log('login successfully');
+    }, error => {
+      console.log('failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+  loggedOut() {
+      localStorage.removeItem('token');
+      console.log('logged out successfully');
+  }
+}
